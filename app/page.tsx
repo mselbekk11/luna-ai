cb5358d620356b6e6cd797e1cce7660ec0601b4a
mselbekk11 <mselbekk11@gmail.com>
2025-05-13 22:17:25 -0700

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { Authenticated, Unauthenticated } from "convex/react";
-// import { api } from "../convex/_generated/api";
-// import Link from "next/link";
-import { SignUpButton } from "@clerk/nextjs";
-import { SignInButton } from "@clerk/nextjs";
-import { UserButton } from "@clerk/nextjs";
+// import { SignUpButton } from "@clerk/nextjs";
+// import { SignInButton } from "@clerk/nextjs";
+import HeroSection from "@/components/hero-section";
 
 export default function Home() {
   return (
     <>
-      <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
+      {/* <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
         Convex + Next.js + Clerk
         <div className="flex flex-row gap-2">
           <SignInButton>
@@ -20,43 +17,16 @@ export default function Home() {
           <SignUpButton>
             <button className="">Sign Up</button>
           </SignUpButton>
-          <UserButton />
         </div>
-      </header>
+      </header> */}
       <main className="p-8 flex flex-col gap-8">
-        <Authenticated>
+        {/* <Authenticated>
           <Content />
-        </Authenticated>
-        <Unauthenticated>
-          <h1>You are not signed in</h1>
-        </Unauthenticated>
+        </Authenticated> */}
+        <HeroSection />
+        {/* <Unauthenticated>
+        </Unauthenticated> */}
       </main>
     </>
-  );
-}
-
-// function SignInForm() {
-//   return (
-//     <div className="flex flex-col gap-8 w-96 mx-auto">
-//       <p>Log in to see the numbers</p>
-//       <SignInButton>
-//         <button className="bg-foreground text-background px-4 py-2 rounded-md">
-//           Sign in
-//         </button>
-//       </SignInButton>
-//       <SignUpButton>
-//         <button className="bg-foreground text-background px-4 py-2 rounded-md">
-//           Sign up
-//         </button>
-//       </SignUpButton>
-//     </div>
-//   );
-// }
-
-function Content() {
-  return (
-    <div className="flex flex-col gap-8 max-w-lg mx-auto">
-      <h1>You are signed in</h1>
-    </div>
   );
 }
