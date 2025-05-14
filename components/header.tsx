57e63e2fc27b6dd752bd347f45247d8647e9f00c
mselbekk11 <mselbekk11@gmail.com>
2025-05-13 22:31:04 -0700

    updates

--- components/header.tsx
+++ components/header.tsx
@@ -7,12 +7,12 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import { SignInButton, SignUpButton } from "@clerk/clerk-react";
 
-const menuItems = [
-  { name: "Features", href: "#link" },
-  { name: "Solution", href: "#link" },
-  { name: "Pricing", href: "#link" },
-  { name: "About", href: "#link" },
-];
+// const menuItems = [
+//   { name: "Features", href: "#link" },
+//   { name: "Solution", href: "#link" },
+//   { name: "Pricing", href: "#link" },
+//   { name: "About", href: "#link" },
+// ];
 
 export const HeroHeader = () => {
   const [menuState, setMenuState] = React.useState(false);
@@ -58,7 +58,7 @@ export const HeroHeader = () => {
               </button>
             </div>
 
-            <div className="absolute inset-0 m-auto hidden size-fit lg:block">
+            {/* <div className="absolute inset-0 m-auto hidden size-fit lg:block">
               <ul className="flex gap-8 text-sm">
                 {menuItems.map((item, index) => (
                   <li key={index}>
@@ -71,10 +71,10 @@ export const HeroHeader = () => {
                   </li>
                 ))}
               </ul>
-            </div>
+            </div> */}
 
             <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
-              <div className="lg:hidden">
+              {/* <div className="lg:hidden">
                 <ul className="space-y-6 text-base">
                   {menuItems.map((item, index) => (
                     <li key={index}>
@@ -87,7 +87,7 @@ export const HeroHeader = () => {
                     </li>
                   ))}
                 </ul>
-              </div>
+              </div> */}
               <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
                 <SignInButton>
                   <Button
