8e735417a114f101d997b7046a20055917dfa3f1
mselbekk11 <mselbekk11@gmail.com>
2025-05-13 22:23:04 -0700

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,12 @@
 "use client";
 
-// import { SignUpButton } from "@clerk/nextjs";
-// import { SignInButton } from "@clerk/nextjs";
 import HeroSection from "@/components/hero-section";
 
 export default function Home() {
   return (
     <>
-      {/* <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
-        Convex + Next.js + Clerk
-        <div className="flex flex-row gap-2">
-          <SignInButton>
-            <button>Sign in</button>
-          </SignInButton>
-
-          <SignUpButton>
-            <button className="">Sign Up</button>
-          </SignUpButton>
-        </div>
-      </header> */}
-      <main className="p-8 flex flex-col gap-8">
-        {/* <Authenticated>
-          <Content />
-        </Authenticated> */}
+      <main className="">
         <HeroSection />
-        {/* <Unauthenticated>
-        </Unauthenticated> */}
       </main>
     </>
   );
