aaedc1489529d8a8ac924624861aa6276681a36a
mselbekk11 <mselbekk11@gmail.com>
2025-05-15 13:30:04 -0700

    updates

--- components/OnboardingCard.tsx
+++ components/OnboardingCard.tsx
@@ -63,23 +63,31 @@ export default function OnboardingCard({
         totalSteps,
       }}
     >
-      <div className="bg-[#0D0D0E] rounded-lg shadow-md p-6 w-full max-w-4xl relative border border-[#383A42] min-h-[650px] flex flex-col">
+      <div className="rounded-lg shadow-md p-6 w-full max-w-4xl relative border border-[#383A42] min-h-[650px] flex flex-col overflow-hidden bg-[#0D0D0E] backdrop-blur-sm">
+        <div
+          aria-hidden
+          className="absolute inset-0 isolate opacity-45 contain-strict z-0"
+        >
+          <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
+          <div className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
+          <div className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
+        </div>
         {/* Back button in the top left */}
         {step > 1 && (
           <button
             onClick={handlePrev}
-            className="absolute top-4 left-4 text-sm text-gray-600 hover:text-gray-800 transition"
+            className="absolute top-4 left-4 text-sm text-gray-300 hover:text-white transition z-10"
           >
             ← Back
           </button>
         )}
 
-        <div className="space-y-4 flex-grow flex flex-col justify-center">
+        <div className="space-y-4 flex-grow flex flex-col justify-center z-1 relative">
           {children}
         </div>
 
         {/* Step indicators as circles in a row - fixed at bottom */}
-        <div className="flex justify-center space-x-3 absolute bottom-8 left-0 right-0">
+        <div className="flex justify-center space-x-3 absolute bottom-8 left-0 right-0 z-1">
           {Array.from({ length: totalSteps }, (_, i) => (
             <div
               key={i}
