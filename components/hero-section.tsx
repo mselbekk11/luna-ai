4a498f2a9f46d34efd37d2a74bcc4e862368e053
mselbekk11 <mselbekk11@gmail.com>
2025-05-16 10:11:13 -0700

    updates

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -89,7 +89,7 @@ export default function HeroSection() {
                     className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950"
                   >
                     <span className="text-foreground text-sm">
-                      AI-Powered Product Formulation Software.
+                      AI-Powered Product Formulation Software
                     </span>
                     <span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700"></span>
 
